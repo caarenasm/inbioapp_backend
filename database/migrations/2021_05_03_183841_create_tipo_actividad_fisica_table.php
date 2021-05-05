6f3a5c21cbe1207e2ecc0f4f1afa033bddb8da87
InbionovaDev <bdagrosol@gmail.com>
2021-05-05 10:18:39 -0500

    IN-121 Creación de la migración tipo de actividad fisica con sus respectivos campos

---
+++ database/migrations/2021_05_03_183841_create_tipo_actividad_fisica_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTipoActividadFisicaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tipo_actividad_fisica', function (Blueprint $table) {
+            $table->id();
+            $table->string('tipo_act',100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tipo_actividad_fisica');
+    }
+}

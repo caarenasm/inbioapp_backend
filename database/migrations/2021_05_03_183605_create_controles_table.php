7d844200ec60af97225961330784adf9a1f7ee64
InbionovaDev <bdagrosol@gmail.com>
2021-05-05 08:18:31 -0500

    IN-121 Creación de la migración controles con sus respectivos campos

---
+++ database/migrations/2021_05_03_183605_create_controles_table.php
@@ -0,0 +1,40 @@
+
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateControlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('controles', function (Blueprint $table) {
+            $table->id();
+            $table->string('telefono',10);
+            $table->string('jornada',50);
+            $table->text('mensaje');
+            $table->string('tipo_contacto',50);
+            
+           
+            $table->foreignId('user_id')
+            ->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('controles');
+    }
+}

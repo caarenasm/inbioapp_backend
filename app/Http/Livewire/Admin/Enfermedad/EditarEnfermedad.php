b84e6e8c142f726abed6c1440665e8cb5af13e6f
InbionovaDev <bdagrosol@gmail.com>
2021-05-07 15:18:32 -0500

    IN-122 Se crea controlador para editar enfermedad (Livewire)

---
+++ app/Http/Livewire/Admin/Enfermedad/EditarEnfermedad.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\EditarEnfermedad;
+
+use Livewire\Component;
+
+class EditarEnfermedad extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.enfermedad.editar-enfermedad');
+    }
+}

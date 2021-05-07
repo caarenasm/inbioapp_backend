05245d32e1eb25e78b2f8a5d9651e275b73eba2a
InbionovaDev <bdagrosol@gmail.com>
2021-05-07 15:16:34 -0500

    IN-122 Se crea controlador para editar alimentos (Livewire)

---
+++ app/Http/Livewire/Admin/Alimentos/EditarAlimentos.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\EditarAlimentos;
+
+use Livewire\Component;
+
+class EditarAlimentos extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.alimentos.editar-alimentos');
+    }
+}

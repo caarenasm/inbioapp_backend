4a891d7f0074a1fbbbf41e1f07a7b1287fbd3ca8
InbionovaDev <bdagrosol@gmail.com>
2021-05-07 15:17:34 -0500

    IN-122 Se crea controlador para editar categoria de alimentos (Livewire)

---
+++ app/Http/Livewire/Admin/Categalimentos/EditarCatgAlimentos.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\EditarCargAlimentos;
+
+use Livewire\Component;
+
+class EditarCatgAlimentos extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.categalimentos.editar-catg-alimentos');
+    }
+}

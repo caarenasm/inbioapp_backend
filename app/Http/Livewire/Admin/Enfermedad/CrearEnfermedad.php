8678266e98f96c1c1fd8fa3998005df99e55d437
InbionovaDev <bdagrosol@gmail.com>
2021-05-07 15:18:15 -0500

    IN-122 Se crea controlador para crear enfermedad (Livewire)

---
+++ app/Http/Livewire/Admin/Enfermedad/CrearEnfermedad.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\CrearEnfermedad;
+
+use Livewire\Component;
+
+class CrearEnfermedad extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.enfermedad.crear-enfermedad');
+    }
+}

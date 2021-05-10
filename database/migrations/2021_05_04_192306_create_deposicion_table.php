1e4ba55d2fd5681da28b9dc56006abb6c511aeae
InbionovaDev <bdagrosol@gmail.com>
2021-05-10 15:49:39 -0500

    IN-142 Se modifica el nombre del atributo tipo_depos a  tipo_deposicion de la migracion deposicion Se modifica el nombre del atributo cantidad_dep a  cantidad_deposicion de la migracion deposicion Se modifica el nombre del atributo bioproducto_a_id a   bioproducto_asignado_id  de la migracion deposicion

--- database/migrations/2021_05_04_192306_create_deposicion_table.php
+++ database/migrations/2021_05_04_192306_create_deposicion_table.php
@@ -15,15 +15,15 @@ class CreateDeposicionTable extends Migration
     {
         Schema::create('deposicion', function (Blueprint $table) {
             $table->id();
-            $table->string('tipo_depos',100)->nullable();
+            $table->string('tipo_deposicion',100)->nullable();
             $table->string('color',50)->nullable();
-            $table->integer('cantidad_dep')->nullable()->default(0);
+            $table->integer('cantidad_deposicion')->nullable()->default(0);
             
             $table->foreignId('medicamento_id')->nullable()
             ->constrained('medicamento')
             ->onDelete('cascade');
 
-            $table->foreignId('bioproducto_a_id')->nullable()
+            $table->foreignId('bioproducto_asignado_id')->nullable()
             ->constrained('bioproducto_asignado')
             ->onDelete('cascade');
 

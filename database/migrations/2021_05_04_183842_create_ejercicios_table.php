e03ece6f01fe4a4cf825b736fd02c8709240fbd4
InbionovaDev <bdagrosol@gmail.com>
2021-05-14 14:13:20 -0500

    IN-142 Se cambia el  nombre de la migracion en plural.

---
+++ database/migrations/2021_05_04_183842_create_ejercicios_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEjerciciosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ejercicios', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo',100)->nullable();
+            $table->text('descripcion')->nullable();
+            $table->string('url',100)->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ejercicios');
+    }
+}

a675f705c04358610c1513a3c0b6bf05d38a45f6
DESKTOP-GIJ6OOM\LIZ <bdagrosol@gmail.com>
2021-05-15 14:41:01 +0200

    IN-145 Se crea vista para listar alimentos.

---
+++ resources/views/livewire/admin/alimentos/listar-alimentos.blade.php
@@ -0,0 +1,53 @@
+<table class="min-w-full divide-y divide-gray-200 ">
+    <thead class="bg-gray-50">
+        <tr>
+            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Alimento
+            </th>
+            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Categoria
+            </th>
+            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Acciones
+            </th>
+        </tr>
+    </thead>
+    <Tbody>
+    <tbody class="bg-white divide-y divide-gray-200">
+        @foreach ($alimentos as $food)
+        <tr>
+            <td class="px-6 py-4 whitespace-nowrap">
+                <div class="flex items-center">
+                    <div class="ml-1">
+                        <div class="text-sm font-medium text-gray-900">
+                            {{$food->nombre}}
+                        </div>
+                    </div>
+                </div>
+            </td>
+            <td class="px-6 py-4 whitespace-nowrap">
+                <div class="flex items-center">
+                    <div class="ml-1">
+                        <div class="text-sm font-medium text-gray-900">
+                            {{$food->nombre_categoria}}
+                        </div>
+                    </div>
+                </div>
+            </td>
+            <td class="px-4 py-4 whitespace-nowrap">
+                <div class="flex items-center">
+                    <h2 class="text-2xl text-fondo-verde font-extrabold" style="margin-right: 10px" >
+                        <x-html.link href="{{ route('alimentos.edit',$food) }}" text="Editar" isButton="true"
+                            class="inline-block"/>
+                    </h2>
+                    <h2>
+                        <a href="{{ route('alimentos.delete', $food->id) }}"
+                            x-on:click="confirmDialog = confirmDialog !== true"
+                            class=" py-2.5 px-2.5 text-sm rounded-md text-white bg-color-peligro eliminar">Eliminar</a>
+                    </h2>
+                </div>
+            </td>
+        </tr>
+        @endforeach
+    </Tbody>
+</table>

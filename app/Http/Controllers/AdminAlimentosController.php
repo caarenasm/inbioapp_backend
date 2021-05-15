c10ae8a67c7688b9b43d8f9af85579a559ab9a74
DESKTOP-GIJ6OOM\LIZ <bdagrosol@gmail.com>
2021-05-15 14:40:00 +0200

    IN-145 Se crea CRUD para alimentos

--- app/Http/Controllers/AdminAlimentosController.php
+++ app/Http/Controllers/AdminAlimentosController.php
@@ -3,12 +3,67 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Alimento;
+use App\Models\CategoriaAlimento;
 use Illuminate\Http\Request;
 
 class AdminAlimentosController extends Controller
 {
-    public function __invoke()
+    public function index()
     {
-        return view('livewire.admin.alimentos.alimentos');
+
+        $alimentos = CategoriaAlimento::join('alimentos', 'categoria_alimento_id', '=', 'categoria_alimentos.id')
+        ->select("*")
+        ->get();
+        return view('livewire.admin.alimentos.alimentos', ['alimentos' => $alimentos]);
+        // return $alimentos;
+    }
+
+    public function create()
+    {
+        $category_food = CategoriaAlimento::all();
+        return view('livewire.admin.alimentos.crear-alimentos', ['category_food' => $category_food]);
+        // return $category_food;
+    }
+
+    public function store(Request $request)
+    {
+
+        $alimentos = new Alimento();
+
+        $alimentos->nombre = $request->nombre;
+        $alimentos->categoria_alimento_id = $request->categoria_alimento_id;
+        $alimentos->save();
+        // return $alimentos;
+        return redirect()->route('alimentos');
+    }
+
+    public function edit(Alimento $food)
+    {
+        $alimentos = CategoriaAlimento::join('alimentos', 'categoria_alimento_id', '=', 'categoria_alimentos.id')
+        ->select("*")
+        ->get();
+        $category_food = CategoriaAlimento::all();
+        return view('livewire.admin.alimentos.editar-alimentos', ['alimentos' => $alimentos, 'food' => $food, 'category_food'=> $category_food]);
+        // return $alimentos;
+    }
+
+    public function update(Request $request, Alimento $food)
+    {
+        $request->validate([
+            'nombre' => 'required',
+            'categoria_alimento_id' => 'required'
+        ]);
+
+        $food->nombre = $request->nombre;
+        $food->categoria_alimento_id = $request->categoria_alimento_id;
+        $food->save();
+        return redirect()->route('alimentos');
+    }
+
+    public function destroy($id)
+    {
+        Alimento::destroy($id);
+        return redirect()->route('alimentos');
     }
 }

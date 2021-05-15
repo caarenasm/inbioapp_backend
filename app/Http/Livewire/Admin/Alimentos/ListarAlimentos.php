7396d169861521a18f1276e15b73ccd45dd45c94
DESKTOP-GIJ6OOM\LIZ <bdagrosol@gmail.com>
2021-05-15 14:38:36 +0200

    IN-145 Se crea controlador de Livewire para listar alimentos

---
+++ app/Http/Livewire/Admin/Alimentos/ListarAlimentos.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ListarAlimentos extends Component
+{
+    public function render()
+    {
+        return view('livewire.listar-alimentos');
+    }
+}

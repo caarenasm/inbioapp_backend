9ce2733630a83a37b45a9e41f811ac3ae0301d74
DESKTOP-GIJ6OOM\LIZ <bdagrosol@gmail.com>
2021-05-16 09:23:26 -0500

    IN-146 Controlador para  planes en livewire (dashborad)

---
+++ app/Http/Livewire/Admin/Planes/Planes.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Planes extends Component
+{
+    public function render()
+    {
+        return view('livewire.planes');
+    }
+}

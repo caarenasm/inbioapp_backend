c039653ea4a555ce9c6bbe177dde7e325a05a0d0
InbionovaDev <bdagrosol@gmail.com>
2021-05-20 10:09:23 -0500

    IN-147 Se Crea la migracion antojos y se  añade el campo pregunta_id para asociar una pregunta a la tabla que tiene relacion con el quiz.

---
+++ database/migrations/2021_05_19_175555_antojos.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Query\Expression;
+
+class Antojos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('antojos', function (Blueprint $table) {
+
+            $table->id();
+
+            $table->string('frecuencia',100);
+
+            $table->json('alimentos')->default(new Expression('(JSON_ARRAY())'));
+
+            $table->foreignId('pregunta_id')->references('id')->on('preguntas')->onDelete('cascade');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('antojos');
+    }
+}

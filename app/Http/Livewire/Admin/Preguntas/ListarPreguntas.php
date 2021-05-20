9dd0c64786ddfbd775435c631049bc08879130d0
InbionovaDev <bdagrosol@gmail.com>
2021-05-20 10:26:38 -0500

    IN-147 Controlador de la vista listar-pregunta (Livewire)

---
+++ app/Http/Livewire/Admin/Preguntas/ListarPreguntas.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Preguntas;
+
+use Livewire\Component;
+
+class ListarPreguntas extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.preguntas.listar-preguntas');
+    }
+}

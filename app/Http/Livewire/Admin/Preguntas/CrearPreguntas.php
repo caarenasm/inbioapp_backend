b7b8f1f48a6d10fc68e5039fba73eada0074562d
InbionovaDev <bdagrosol@gmail.com>
2021-05-20 10:26:22 -0500

    IN-147 Controlador de la vista crear-pregunta (Livewire)

---
+++ app/Http/Livewire/Admin/Preguntas/CrearPreguntas.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Preguntas;
+
+use Livewire\Component;
+
+class CrearPreguntas extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.preguntas.crear-preguntas');
+    }
+}

883c01657fdf228b43e2db7d09d25d452f7b9758
InbionovaDev <bdagrosol@gmail.com>
2021-05-20 10:26:29 -0500

    IN-147 Controlador de la vista editar-pregunta (Livewire)

---
+++ app/Http/Livewire/Admin/Preguntas/EditarPreguntas.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Preguntas;
+
+use Livewire\Component;
+
+class EditarPreguntas extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.preguntas.editar-preguntas');
+    }
+}

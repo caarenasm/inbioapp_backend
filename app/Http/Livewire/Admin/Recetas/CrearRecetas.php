d7d47c1ed6fa89a7452e1302a9aef001100492ea
DESKTOP-GIJ6OOM\LIZ <bdagrosol@gmail.com>
2021-05-22 07:53:04 -0500

    IN-214 Controlador para crear recetas (Livewire)

---
+++ app/Http/Livewire/Admin/Recetas/CrearRecetas.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class CrearRecetas extends Component
+{
+    public function render()
+    {
+        return view('livewire.crear-recetas');
+    }
+}

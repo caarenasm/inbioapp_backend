bcb996588c867e441fb64428f2211eb21826343d
DESKTOP-GIJ6OOM\LIZ <bdagrosol@gmail.com>
2021-05-22 07:53:33 -0500

    IN-214 Controlador para listar recetas (Livewire)

---
+++ app/Http/Livewire/Admin/Recetas/ListarRecetas.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ListarRecetas extends Component
+{
+    public function render()
+    {
+        return view('livewire.listar-recetas');
+    }
+}

c571f462ed5f125f86fbf7b7674853469e733dc8
DESKTOP-GIJ6OOM\LIZ <bdagrosol@gmail.com>
2021-05-22 07:52:00 -0500

    IN-214 Controlador para crear ingredientes (Livewire)

---
+++ app/Http/Livewire/Admin/Ingredientes/CrearIngredientes.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class CrearIngredientes extends Component
+{
+    public function render()
+    {
+        return view('livewire.crear-ingredientes');
+    }
+}

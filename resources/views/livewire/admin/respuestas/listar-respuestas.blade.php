48c9e6f49e575fbfc6e45e2f47058430805ba4c5
DESKTOP-GIO8CC4\DESARROLLO INBIONOVA <bdagrosol@gmail.com>
2021-05-26 08:55:14 -0500

    IN-222 Vistas livewire para  Listar respuestas

---
+++ resources/views/livewire/admin/respuestas/listar-respuestas.blade.php
@@ -0,0 +1,76 @@
+<table class="min-w-full divide-y divide-gray-200 ">
+    <thead class="bg-gray-50">
+        <tr>
+            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                id
+            </th>
+            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Pregunta
+            </th>
+            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Respuesta
+            </th>
+            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Ayuda
+            </th>
+            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Acciones
+            </th>
+        </tr>
+    </thead>
+    <Tbody>
+    <tbody class="bg-white divide-y divide-gray-200">
+    @foreach($respuestas as $answer)
+        <tr>
+            <td class="px-6 py-4 whitespace-nowrap">
+                <div class="flex items-center">
+                    <div class="ml-1">
+                        <div class="text-sm font-medium text-gray-900">
+                            {{$answer->id}}
+                        </div>
+                    </div>
+                </div>
+            </td>
+            <td class="px-6 py-4 whitespace-nowrap">
+                <div class="flex items-center">
+                    <div class="ml-1">
+                        <div class="text-sm font-medium text-gray-900">
+                            {{$answer->pregunta}}
+                        </div>
+                    </div>
+                </div>
+            </td>
+            <td class="px-6 py-4 whitespace-nowrap">
+                <div class="flex items-center">
+                    <div class="ml-1">
+                        <div class="text-sm font-medium text-gray-900">
+                            {{$answer->respuesta}}
+                        </div>
+                    </div>
+                </div>
+            </td>
+            <td class="px-6 py-4 whitespace-nowrap">
+                <div class="flex items-center">
+                    <div class="ml-1">
+                        <div class="text-sm font-medium text-gray-900">
+                            {{$answer->ayuda}}
+                        </div>
+                    </div>
+                </div>
+            </td>
+            <td class="px-6 py-4 whitespace-nowrap">
+                <div class="flex items-center">
+                    <h2 class="text-2xl text-fondo-verde font-extrabold" style="margin-right: 0.5em">
+                        <x-html.link href="{{route('respuestas.edit',$answer)}}" text="Editar" isButton="true" class="inline-block mb-2 ml-1" />
+                    </h2>
+                    <h2 style="margin-bottom: 0.5em">
+                        <a href="{{ route('respuestas.delete', $answer->id) }}"
+                            x-on:click="confirmDialog = confirmDialog !== true"
+                            class=" py-2.5 px-2.5 text-sm rounded-md text-white bg-color-peligro eliminar">Eliminar</a>
+                    </h2>
+                </div>
+            </td>
+        </tr>
+        @endforeach
+    </Tbody>
+</table>

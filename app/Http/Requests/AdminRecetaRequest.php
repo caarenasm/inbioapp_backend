a5b0de29bb6b45afa301c0450c7b00a0e6e7e2c4
DESKTOP-GIO8CC4\DESARROLLO INBIONOVA <bdagrosol@gmail.com>
2021-06-01 11:23:20 -0500

    IN-249 Se crea la clase AdminRecetaRequest para las validaciones.

---
+++ app/Http/Requests/AdminRecetaRequest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AdminRecetaRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $receta = $this->route()->parameter('receta');
+
+        $rules = [
+            'titulo' => 'required',
+            'slug' => 'required|unique:recetas',
+            'publicacion' => 'required|in:0,1'
+        ];
+
+        if ($receta) {
+            $rules['slug'] = 'required|unique:recetas,slug,' . $receta->id;
+        }
+
+        if ($this->publicacion == 1) {
+            $rules = array_merge($rules, [
+                'seo_titulo' => 'required',
+                'seo_descripcion' => 'required',
+                'descripcion' => 'required',
+                'preparacion' => 'required',
+                'fecha_publicacion' => 'required',
+                'caloria' => 'required',
+                'grasa' => 'required',
+                'proteina' => 'required',
+                'imagen_url' => 'required'
+            ]);
+        }
+        return $rules;
+    }
+}

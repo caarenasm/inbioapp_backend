6ea288ba37cdca37fb4d220a1d393265438e7677
DESKTOP-GIO8CC4\DESARROLLO INBIONOVA <bdagrosol@gmail.com>
2021-06-01 14:47:22 -0500

    IN-250 Se modifica la interfaz de creacion incluyendo el campo slug y se modifica el campo descripcion.

--- resources/views/livewire/admin/planes/crear-planes.blade.php
+++ resources/views/livewire/admin/planes/crear-planes.blade.php
@@ -1,41 +1,53 @@
 <x-app-layout>
     @section('title', 'Crear categoria de alimentos')
 
-        <div class="p-2 bg-white">
+        <div class="p-2 bg-white pt-6 m-2">
             <h2 class="text-2xl py-2 text-fondo-verde font-extrabold">Crear nuevo plan</h2>
             <div class="flex flex-col">
                 <form method="post" action="{{ route('planes.store') }}" accept-charset="UTF-8"
                     enctype="multipart/form-data">
                     @csrf
-                    <div class="space-x-2 w-96">
+                    <div class="space-x-2 w-1/2">
                         <div class="mb-3">
                             <div class="mb-3">
                                 <label for="titulo" class="block font-bold text-gray-700">Titulo</label>
                                 <input type="text" name="titulo" id="titulo"
                                     class="w-full rounded-xl text-gray-500 border-gray-300" value="">
-                            </div>
-                            <div class="mb-3">
-                                <label for="descripcion" class="block font-bold text-gray-700">Descripción del plan</label>
-                                @error('description')
+                                @error('titulo')
                                     <small class="text-red-500">* {{ $message }}</small>
                                 @enderror
+                            </div>
+                            <div class="mb-3">
+                                <label for="slug" class="block font-bold text-gray-700">Url de la entrada</label>
+                                <input type="text" name="slug" id="slug"
+                                    class="w-full rounded-xl text-gray-500 border-gray-300" value="" readonly>
+                                @error('slug')
+                                    <small class="text-red-500">* {{ $message }}</small>
+                                @enderror
+                            </div>
+                            <div class="w-full mb-3">
+                                <label for="descripcion" class="block font-bold text-gray-700">Descripcion</label>
                                 <textarea name="descripcion" id="descripcion"
                                     class="w-full rounded-xl text-gray-500 border-gray-300"></textarea>
+                                @error('descripcion')
+                                    <small class="text-red-500">* {{ $message }}</small>
+                                @enderror
                             </div>
                             <div class="grid grid-cols-2 gap-4 mb-3">
                                 <div class="col">
                                     <div class="image-wrapper">
-                                        <img id="picture"
-                                            src="{{ asset('./imagenes/imagenes-planes/placeholder.png') }}"
-                                            alt="">
+                                        <img id="picture" src="{{ asset('./imagenes/planes/placeholder.png') }}" alt="">
                                     </div>
                                 </div>
 
                                 <div class="col">
                                     <div class="form-group">
                                         {!! Form::label('imagen_url', 'Selecciona la imagen') !!}
-                                        {!! Form::file('imagen_url', ['class' => 'form-control-file']) !!}
+                                        <input type="file" name="imagen_url" id="imagen_url" class="form-control-file">
                                     </div>
+                                    @error('imagen_url')
+                                        <small class="text-red-500">* {{ $message }}</small>
+                                    @enderror
                                 </div>
                             </div>
 
@@ -43,7 +55,7 @@
                                 <label for="precio" class="block font-bold text-gray-700">Precio</label>
                                 <input type="number" name="precio" id="precio"
                                     class="w-full rounded-xl text-gray-500 border-gray-300" value="">
-                                @error('price')
+                                @error('precio')
                                     <small class="text-red-500">* {{ $message }}</small>
                                 @enderror
                             </div>
@@ -79,6 +91,7 @@
 
         </style>
     @endsection
+
     @push('scripts')
         <script>
             document.getElementById("imagen_url").addEventListener('change', cambiarImagen);
@@ -95,6 +108,104 @@
             }
 
         </script>
+
+        <script src="/js/ckeditor5.js"></script>
+
+        <script>
+            document.addEventListener('livewire:load', function() {
+                const titulo = document.getElementById('titulo');
+                const slug = document.getElementById('slug');
+                titulo.onblur = function() {
+                    slug.value = slugify(titulo.value);
+                }
+                titulo.onkeydown = function() {
+                    slug.value = slugify(titulo.value);
+                }
+                titulo.onkeyup = function() {
+                    slug.value = slugify(titulo.value);
+                }
+
+                // CK Editor
+                ClassicEditor
+                    .create(document.querySelector('#descripcion'), {
+                        toolbar: {
+                            items: [
+                                'heading',
+                                '|',
+                                'bold',
+                                'italic',
+                                'link',
+                                'bulletedList',
+                                'numberedList',
+                                '|',
+                                'imageUpload',
+                                'blockQuote',
+                                'insertTable',
+                                'mediaEmbed',
+                                'undo',
+                                'redo'
+                            ]
+                        },
+                        language: 'es',
+                        image: {
+                            toolbar: ['imageStyle:alignLeft', 'imageStyle:alignCenter', 'imageStyle:alignRight',
+                                '|',
+                                'resizeImage',
+                                '|',
+                                'imageTextAlternative'
+                            ],
+                            styles: [
+                                'alignLeft', 'alignCenter', 'alignRight'
+                            ],
+                        },
+                        table: {
+                            contentToolbar: [
+                                'tableColumn',
+                                'tableRow',
+                                'mergeTableCells'
+                            ]
+                        },
+                        simpleUpload: {
+                            uploadUrl: '{{ asset('./imagenes/planes/placeholder.png') }}',
+                            headers: {
+
+                            }
+                        },
+                    })
+                    .then(editor => {
+                        window.editor = editor;
+                    })
+                    .catch(error => {
+                        console.error('Oops, something went wrong!');
+                        console.error(
+                            'Please, report the following error on https://github.com/ckeditor/ckeditor5/issues with the build id and the error stack trace:'
+                        );
+                        console.warn('Build id: smh51lc3zo1f-qavakagvqr26');
+                        console.error(error);
+                    });
+                // CK Editor fin
+            });
+
+            function slugify(str) {
+                str = str.replace(/^\s+|\s+$/g, ''); // trim
+                str = str.toLowerCase();
+
+                // remove accents, swap ñ for n, etc
+                var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
+                var to = "aaaaeeeeiiiioooouuuunc------";
+
+                for (var i = 0, l = from.length; i < l; i++)
+                    str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+
+
+                str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+                    .replace(/\s+/g, '-') // collapse whitespace and replace by -
+                    .replace(/-+/g, '-'); // collapse dashes
+
+                return str;
+            }
+
+        </script>
     @endpush
-    
+
 </x-app-layout>

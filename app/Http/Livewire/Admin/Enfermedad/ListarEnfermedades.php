95125af28d70b53614d031615c785bdd8dcb9c18
DESKTOP-GIO8CC4\DESARROLLO INBIONOVA <bdagrosol@gmail.com>
2021-06-10 17:01:08 -0500

    IN-260 Se crea controlador livewire listar enfermedades

---
+++ app/Http/Livewire/Admin/Enfermedad/ListarEnfermedades.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ListarEnfermedades extends Component
+{
+    public function render()
+    {
+        return view('livewire.listar-enfermedades');
+    }
+}

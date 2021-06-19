c89d07603e96e11cf805f0b1cbf0993aabc38c2a
DESKTOP-GIJ6OOM\LIZ <bdagrosol@gmail.com>
2021-06-18 22:18:02 -0500

    IN-261 Se crea controlador livewire para Listar Semaforo estado

---
+++ app/Http/Livewire/Admin/SemaforoEstado/SemaforoEstado.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class SemaforoEstado extends Component
+{
+    public function render()
+    {
+        return view('livewire.semaforo-estado');
+    }
+}

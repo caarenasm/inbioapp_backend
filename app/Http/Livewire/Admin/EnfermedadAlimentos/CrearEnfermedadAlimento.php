433ccab9a8aaa0e003d2b3e2a6025764e4af4b42
DESKTOP-GIJ6OOM\LIZ <bdagrosol@gmail.com>
2021-06-19 14:34:48 -0500

    IN-261 Se añade metodos para renderizar componentes pero no funcionaron. Por ende estan documentados.

--- app/Http/Livewire/Admin/EnfermedadAlimentos/CrearEnfermedadAlimento.php
+++ app/Http/Livewire/Admin/EnfermedadAlimentos/CrearEnfermedadAlimento.php
@@ -10,4 +10,20 @@ class CrearEnfermedadAlimento extends Component
     {
         return view('livewire.crear-enfermedad-alimento');
     }
+
+    // public $selectedCategoria = null, $selectedAlimento = null;
+
+    // public $almentos = null;
+
+    // public function render()
+    // {
+    //     return view('livewire.crear-enfermedad-alimento',[
+    //         'categorias' => CategoriaAlimento::all(),
+    //     ]);
+    // }
+
+    // public function updatedselectedCategoria($categoria_id)
+    // {
+    //     $this->alimentos = Alimento::where('categoria_alimento_id',$categoria_id)->get();
+    // }
 }

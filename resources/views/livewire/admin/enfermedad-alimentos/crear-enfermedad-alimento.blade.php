66fc53b288ab5c5936c799107c2b79b3ff3050bc
DESKTOP-GIJ6OOM\LIZ <bdagrosol@gmail.com>
2021-06-19 14:36:00 -0500

    IN-261 Se cambia toda la estructura de la vista Crear Enfermedad Alimento

--- resources/views/livewire/admin/enfermedad-alimentos/crear-enfermedad-alimento.blade.php
+++ resources/views/livewire/admin/enfermedad-alimentos/crear-enfermedad-alimento.blade.php
@@ -1,4 +1,4 @@
-<form action="{{ route('enfermedades-alimentos.store') }}" method="POST">
+{{-- <form action="{{ route('enfermedades-alimentos.store') }}" method="POST">
     @csrf
     <table class="min-w-full divide-y divide-gray-200" id="table">
         <thead class="bg-gray-50">
@@ -19,7 +19,7 @@
         </thead>
         <Tbody>
         <tbody class="bg-white divide-y divide-gray-200">
-            @for ($i = 0; $i <count($alimentos); $i++)
+            @for ($i = 0; $i < count($alimentos); $i++)
                     <tr>
                         <td class="px-6 py-4 whitespace-nowrap">
                             <div class="flex justify-items-center">
@@ -65,5 +65,52 @@
         </Tbody>
     </table>
     <x-forms.button type="submit" text="Agregar"/>
-</form>
+</form> --}}
 
+@section('title', 'Crear Alimento Enfermedad')
+    <div class="p-5 bg-white w-auto">
+        <h2 class="text-lg py-2 text-fondo-verde font-extrabold">Añade el estado a los alimentos para:
+            {{ $enfermedades->enfermedad }}</h2>
+        <div class="flex flex-col">
+            <form action="{{ route('enfermedades-alimentos.store') }}" method="POST">
+                @csrf
+                <div class="grid grid-cols-2 mt-6">
+                    <div class="w-auto mb-3">
+                        <p class="block font-bold text-gray-700">Elijé el alimento</p>
+                        <select class="form-control" id="alimento_id" name="alimento_id">
+                            <option value="" selected>Escoge el alimento</option>
+                            @foreach ($alimentos as $alimento)
+                                <option value="{{ $alimento->id }}">{{ $alimento->nombre }}</option>
+                            @endforeach
+                        </select>
+                        @error('alimento_id')
+                            <small class="text-red-500">* {{ $message }}</small>
+                        @enderror
+                    </div>
+
+                    <div class="w-auto mb-3">
+                        <p class="block font-bold text-gray-700">Elijé el estado del alimento</p>
+                        <select class="form-control" id="recomendacion" name="recomendacion">
+                            <option value="" selected>Escoge el estado del alimento</option>
+                            @foreach ($estados as $estado)
+                                <option value="{{ $estado->id }}">{{ $estado->estado_semaforo }}</option>
+                            @endforeach
+                        </select>
+                        @error('recomendacion')
+                            <small class="text-red-500">* {{ $message }}</small>
+                        @enderror
+                    </div>
+
+                </div>
+                <div class="w-auto m-6 grid grid-cols-2 gap-4 mt-6">
+                    <input type="hidden" value="{{ $enfermedades->id }}" name="enfermedad_id">
+                    <x-forms.button type="submit" text="Guardar cambios" />
+                    <a href="{{ url()->previous() }}"
+                        class="w-full justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-color-primario hover:bg-color-primario-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-color-primario-700 text-center"
+                        type="submit">
+                        Cancel
+                    </a>
+                </div>
+            </form>
+        </div>
+    </div>

ac1286af548e07e4a35a7ecdb4acf9f65caa9c81
DESKTOP-GIO8CC4\DESARROLLO INBIONOVA <bdagrosol@gmail.com>
2021-07-08 10:26:06 -0500

    IN-337 Se crea vista livewire para listar objetivos

---
+++ resources/views/livewire/admin/objetivos/listar-objetivos.blade.php
@@ -0,0 +1,72 @@
+<table class="min-w-full divide-y divide-gray-200 ">
+    <thead class="bg-gray-50">
+        <tr>
+            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Imagen
+            </th>
+            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Nombre del objetivo
+            </th>
+            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Descripcion
+            </th>
+            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Acciones
+        </tr>
+    </thead>
+    <Tbody>
+    <tbody class="bg-white divide-y divide-gray-200">
+    @foreach($objetivos as $objetivo)
+        <tr>
+            <td class="px-6 py-4 whitespace-nowrap">
+                <div class="flex items-center">
+                    <div class="ml-1">
+                        <div class="text-sm font-medium text-gray-900">
+                            <img id="picture" src="{{ asset('./imagenes/objetivos/' . old('imagen_url', $objetivo->imagen_url)) }}" alt="imagen_objetivo" style="width: 200px; height: 100;">
+                        </div>
+                    </div>
+                </div>
+            </td>
+            <td class="px-6 py-4 whitespace-nowrap">
+                <div class="flex items-center">
+                    <div class="ml-1">
+                        <div class="text-sm font-medium text-gray-900">
+                            {{$objetivo->titulo}}
+                        </div>
+                    </div>
+                </div>
+            </td>
+            <td class="px-6 py-4 whitespace-nowrap">
+                <div class="flex items-center">
+                    <div class="ml-1">
+                        <div class="text-sm font-medium text-gray-900">
+                            {{strip_tags($objetivo->descripcion)}}
+                        </div>
+                    </div>
+                </div>
+            </td>
+            <td class="px-6 py-4 whitespace-nowrap">
+                <div class="flex items-center">
+                    <div class="ml-1">
+                        <div class="text-sm font-medium text-gray-900">
+                            {{$objetivo->precio}}
+                        </div>
+                    </div>
+                </div>
+            </td>
+            <td class="px-4 py-4 whitespace-nowrap">
+                <div class="flex items-center">
+                    <h2 class="text-2xl text-fondo-verde font-extrabold" style="margin-right: 10px">
+                        <x-html.link href="{{route('objetivos.edit',$objetivo)}}" text="Editar" isButton="true" class="inline-block mb-2 ml-1" />
+                    </h2>
+                    <h2 style="margin-bottom: 0.5em">
+                        <a href="{{ route('objetivos.delete', $objetivo->id) }}"
+                            x-on:click="confirmDialog = confirmDialog !== true"
+                            class=" py-2.5 px-2.5 text-sm rounded-md text-white bg-color-peligro eliminar">Eliminar</a>
+                    </h2>
+                </div>
+            </td>
+        </tr>
+        @endforeach
+    </Tbody>
+</table>

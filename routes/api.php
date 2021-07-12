25788910c65d448d41af9bc44db9e1e45fabe827
DESKTOP-GIO8CC4\DESARROLLO INBIONOVA <bdagrosol@gmail.com>
2021-07-12 09:57:40 -0500

    IN-338 Se añaden las rutas para la api de eventos

--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\Api\RecetaController;
 use App\Http\Controllers\Api\BlogApi;
 use App\Http\Controllers\Api\PlanController;
 use App\Http\Controllers\Api\EnfermedadAlimentoController;
+use App\Http\Controllers\api\EventoController;
 use Facade\FlareClient\Api;
 
 /*
@@ -58,6 +59,8 @@ Route::group([
             Route::get('receta', [RecetaController::class, 'lista']);
             Route::get('blog', [BlogApi::class, 'lista']);
             Route::get('plan', [PlanController::class, 'lista']);
+            Route::get('eventos', [EventoController::class, 'tipoEventos']);
+            Route::get('eventos/lista', [EventoController::class, 'lista_eventos']);
             Route::get('logout', [AuthController::class, 'logout']);
             
         });

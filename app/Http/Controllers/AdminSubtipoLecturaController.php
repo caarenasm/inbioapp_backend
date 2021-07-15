de28d902b56bf66fdad00354dd168fb435ab262c
DESKTOP-GIO8CC4\DESARROLLO INBIONOVA <bdagrosol@gmail.com>
2021-07-15 11:46:21 -0500

    IN-343 Se crea controlador para subtipo de lectura

---
+++ app/Http/Controllers/AdminSubtipoLecturaController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\AdminSubtipoLecturaRequest;
+use App\Models\Icon;
+use App\Models\SubtipoLectura;
+use App\Models\TipoLectura;
+use Illuminate\Http\Request;
+
+class AdminSubtipoLecturaController extends Controller
+{
+    public function index()
+    {
+        $tipos_lecturas = TipoLectura::all();
+
+        $iconos = Icon::all();
+
+        $subtipos_lecturas = TipoLectura::join('subtipo_lecturas', 'tipo_lectura_id', '=', 'tipo_lecturas.id')
+        ->select("*")
+        ->get();
+
+        return view('livewire.admin.subtipo-lecturas.subtipo-lecturas', ['iconos'=>$iconos,'subtipos_lecturas' => $subtipos_lecturas,'tipos_lecturas'=>$tipos_lecturas]);
+        // return $subtipos_lecturas;
+    }
+
+    public function store(AdminSubtipoLecturaRequest $request)
+    {
+
+        $subtipo_lectura = new SubtipoLectura();
+
+        $subtipo_lectura->descripcion = $request->descripcion;
+        $subtipo_lectura->tipo_lectura_id = $request->tipo_lectura_id;
+        $subtipo_lectura->icono = $request->icono;
+        $subtipo_lectura->save();
+        return redirect()->route('subtipos-lecturas.index');
+    }
+
+    public function edit(SubtipoLectura $subtipo_lectura)
+    {
+        $tipos_lecturas = TipoLectura::all();
+
+        $iconos = Icon::all();
+
+        $subtipos_lecturas = TipoLectura::join('subtipo_lecturas', 'tipo_lectura_id', '=', 'tipo_lecturas.id')
+        ->select("*")
+        ->get();
+
+        return view('livewire.admin.subtipo-lecturas.editar-subtipo-lecturas', ['subtipos_lecturas' => $subtipos_lecturas, 'subtipo_lectura' => $subtipo_lectura,'tipos_lecturas'=>$tipos_lecturas,'iconos'=>$iconos]);
+    }
+
+    public function update(AdminSubtipoLecturaRequest $request,SubtipoLectura $subtipo_lectura)
+    {
+        $subtipo_lectura->descripcion = $request->descripcion;
+        $subtipo_lectura->tipo_lectura_id = $request->tipo_lectura_id;
+        $subtipo_lectura->icono = $request->icono;
+        $subtipo_lectura->save();
+
+        return redirect()->route('subtipos-lecturas.index');
+    }
+
+    public function destroy($id)
+    {
+        TipoLectura::destroy($id);
+        return redirect()->route('subtipos-lecturas.index');
+    }
+}

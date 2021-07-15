e8d38776c21456825acb0e620520d12285a4ce26
DESKTOP-GIO8CC4\DESARROLLO INBIONOVA <bdagrosol@gmail.com>
2021-07-15 11:55:16 -0500

    IN-343 Se crea controlador livewire para  listar categoria diario

---
+++ app/Http/Livewire/Admin/CategoriaDiario/ListarCategoriasDiarios.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ListarCategoriasDiarios extends Component
+{
+    public function render()
+    {
+        return view('livewire.listar-categorias-diarios');
+    }
+}

4b3de476342175bdb6f0c02649b814987dbe2726
DESKTOP-GIO8CC4\DESARROLLO INBIONOVA <bdagrosol@gmail.com>
2021-07-21 11:50:52 -0500

    IN-356 Se añade validacion para el json de los datos leidos.

--- app/Http/Controllers/Api/LecturaUserController.php
+++ app/Http/Controllers/Api/LecturaUserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\AdminUserTipoLecturaRequest;
 use Illuminate\Http\Request;
 
 //*******agregar esta linea******//
@@ -18,28 +19,67 @@ class LecturaUserController extends Controller
     {
 
         $validator = Validator::make($request->all(), [
-            'user_id'=> 'required',
-            'tipo_lectura_id'=> 'required',
-            'datos_leidos'=> 'required',
+            'user_id' => 'required',
+            'tipo_lectura_id' => 'required',
+            'datos_leidos' => 'required',
         ]);
+
+        if ($request->tipo_lectura_id == 1) {
+
+            $data = json_decode($request->datos_leidos, true);
+            $rules = [
+                'id' => 'required',
+                'calidad_sueño' => 'required', //Must be a number and length of value is 8
+                'hora_inicio' => 'required',
+                'hora_fin' => 'required',
+                'total_horas' => 'required'
+            ];
+
+            $validator = Validator::make($data, $rules);
+            if ($validator->passes()) {
+                //TODO Handle your data
+            } else {
+                //TODO Handle your error
+                dd($validator->errors()->all());
+            }
+        } else{
+            if ($request->tipo_lectura_id == 2) {
+                $data = json_decode($request->datos_leidos, true);
+                $rules = [
+                    'id' => 'required',
+                    'peso_actual' => 'required', //Must be a number and length of value is 8
+                ];
+    
+                $validator = Validator::make($data, $rules);
+                if ($validator->passes()) {
+                    //TODO Handle your data
+                } else {
+                    //TODO Handle your error
+                    dd($validator->errors()->all());
+                }
+            }
+        }
+
 
         if ($validator->fails()) {
             return response()->json([
                 //'message' => $validator->errors()->first(), 
-                'message' => $validator->getMessageBag()->toArray(), 
+                'message' => $validator->getMessageBag()->toArray(),
                 'status' => false
             ], 500);
         }
 
+
+
         DB::beginTransaction();
-  
+
         try {
 
             $lectura_users = new LecturaUser();
             $lectura_users->user_id = $request->User()->id;
             $lectura_users->tipo_lectura_id = $request->tipo_lectura_id;
             $lectura_users->datos_leidos = $request->datos_leidos;
-        
+
             $lectura_users->save();
 
             DB::commit();
@@ -48,8 +88,7 @@ class LecturaUserController extends Controller
                 'message' => 'Usuario creado con exito!',
                 //'data' => $request->all()
             ], 200);
-
-        }catch (\Illuminate\Database\QueryException $e){
+        } catch (\Illuminate\Database\QueryException $e) {
 
             DB::rollback();
 
@@ -63,6 +102,5 @@ class LecturaUserController extends Controller
                 'message' => 'Error en operacion!'
             ], 400);
         }
-
     }
 }

31800186b940d2b14450a3f715f186390c366e4d
DESKTOP-GIO8CC4\DESARROLLO INBIONOVA <bdagrosol@gmail.com>
2021-07-29 16:12:08 -0500

    IN-395 Se crea controlador llivewire listar usuario infomacion

---
+++ app/Http/Livewire/Admin/UsuarioInformacion/ListarUsuariosInformacion.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ListarUsuariosInformacion extends Component
+{
+    public function render()
+    {
+        return view('livewire.listar-usuarios-informacion');
+    }
+}

bacb3993de72458fefb3443efbd4a94c8115f25a
DESKTOP-GIO8CC4\DESARROLLO INBIONOVA <bdagrosol@gmail.com>
2021-08-02 12:49:56 -0500

    IN-396 Se crea vista para listar sueño

---
+++ app/Http/Livewire/Admin/UsuarioInformacion/ListarSueñoEstadisticas.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ListarSueñoEstadisticas extends Component
+{
+    public function render()
+    {
+        return view('livewire.listar-sueño-estadisticas');
+    }
+}

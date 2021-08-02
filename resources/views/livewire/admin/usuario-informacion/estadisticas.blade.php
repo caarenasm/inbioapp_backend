884bf55fea77c72284116d0599dc42ac2c83611b
DESKTOP-GIO8CC4\DESARROLLO INBIONOVA <bdagrosol@gmail.com>
2021-08-02 16:56:40 -0500

    IN-396 Se realiza prueba de switch para cargar componentes

--- resources/views/livewire/admin/usuario-informacion/estadisticas.blade.php
+++ resources/views/livewire/admin/usuario-informacion/estadisticas.blade.php
@@ -11,24 +11,35 @@
                     <option value="{{ $tipo->id }}">{{ $tipo->nombre }}</option>
                 @endforeach
             </select>
-            <div class="grid grid-cols-2 p-2 bg-white">
-                <div class="flex flex-col mb-6">
-                    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                        <div class="py-2 align-middle inline-block sm:px-6 lg:px-8">
-                            <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                                @include('livewire/admin/usuario-informacion/listar-sueño-estadisticas')
+
+            @switch($tipo->id)
+                @case(1)
+                    <div class="flex flex-col mb-6">
+                        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                            <div class="py-2 align-middle inline-block sm:px-6 lg:px-8">
+                                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                                    @include('livewire/admin/usuario-informacion/listar-sueño-estadisticas')
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="flex flex-col">
-                    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                        <div class="py-2 align-middle inline-block sm:px-6 lg:px-8">
-                            <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                                @include('livewire/admin/usuario-informacion/listar-peso-actual-estadisticas')
+                @break
+                @case(2)
+                    <div class="flex flex-col">
+                        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                            <div class="py-2 align-middle inline-block sm:px-6 lg:px-8">
+                                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                                    @include('livewire/admin/usuario-informacion/listar-peso-actual-estadisticas')
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
+                @break
+                @default
+
+            @endswitch
+            {{-- <div class="grid grid-cols-2 p-2 bg-white">
+
+
+            </div> --}}
     </x-app-layout>

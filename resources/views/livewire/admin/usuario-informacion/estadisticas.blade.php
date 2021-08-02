6312293a5cb97597a0e0d5d672d5a55e645e7ed8
DESKTOP-GIO8CC4\DESARROLLO INBIONOVA <bdagrosol@gmail.com>
2021-08-02 16:09:49 -0500

    IN-396 Se añade select para listar tipo de lecturas

--- resources/views/livewire/admin/usuario-informacion/estadisticas.blade.php
+++ resources/views/livewire/admin/usuario-informacion/estadisticas.blade.php
@@ -1,9 +1,16 @@
 <x-app-layout>
 
-    @section('title', 'Tipo de eventos')
+    @section('title', 'Estadisticas')
 
         <div class="p-2 bg-white">
-            <h2 class="text-2xl py-2 text-fondo-verde font-extrabold">Tipo de eventos</h2>
+            <h2 class="text-2xl py-2 text-fondo-verde font-extrabold">Estadisticas del usuario</h2>
+            <p class="block font-bold text-gray-700">Elijé la el tipo de lectura</p>
+            <select wire:model="selectedState" class="form-control" id="tipo_lectura_id" name="tipo_lectura_id">
+                <option value="" selected>Escoge el tipo</option>
+                @foreach ($tipo_lectura as $tipo)
+                    <option value="{{ $tipo->id }}">{{ $tipo->nombre }}</option>
+                @endforeach
+            </select>
             <div class="grid grid-cols-2 p-2 bg-white">
                 <div class="flex flex-col mb-6">
                     <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
@@ -18,7 +25,7 @@
                     <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                         <div class="py-2 align-middle inline-block sm:px-6 lg:px-8">
                             <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                                {{-- @include('livewire/admin/usuario-informacion/listar-usuarios-informacion') --}}
+                                @include('livewire/admin/usuario-informacion/listar-peso-actual-estadisticas')
                             </div>
                         </div>
                     </div>

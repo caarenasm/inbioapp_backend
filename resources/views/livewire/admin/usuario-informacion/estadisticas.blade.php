f99114552c0d6c58f9e21b7ea23fbef02697228c
DESKTOP-GIO8CC4\DESARROLLO INBIONOVA <bdagrosol@gmail.com>
2021-08-02 12:48:15 -0500

    IN-396 Se añade la vista para listar las estadisticas

--- resources/views/livewire/admin/usuario-informacion/estadisticas.blade.php
+++ resources/views/livewire/admin/usuario-informacion/estadisticas.blade.php
@@ -9,7 +9,7 @@
                     <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                         <div class="py-2 align-middle inline-block sm:px-6 lg:px-8">
                             <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                                @include('livewire/admin/tipo-eventos/crear-tipo-eventos')
+                                @include('livewire/admin/usuario-informacion/listar-sueño-estadisticas')
                             </div>
                         </div>
                     </div>
@@ -18,7 +18,7 @@
                     <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                         <div class="py-2 align-middle inline-block sm:px-6 lg:px-8">
                             <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                                @include('livewire/admin/tipo-eventos/listar-tipo-eventos')
+                                {{-- @include('livewire/admin/usuario-informacion/listar-usuarios-informacion') --}}
                             </div>
                         </div>
                     </div>

0673f4de2d0e9800e16d44904f7de4ae8426257c
DESKTOP-GIO8CC4\DESARROLLO INBIONOVA <bdagrosol@gmail.com>
2021-08-06 15:34:10 -0500

    IN-396 Controlador livewire

---
+++ app/Http/Livewire/Admin/UsuarioInformacion/PesoEstadisticas.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class PesoEstadisticas extends Component
+{
+    public function render()
+    {
+        return view('livewire.peso-estadisticas');
+    }
+}

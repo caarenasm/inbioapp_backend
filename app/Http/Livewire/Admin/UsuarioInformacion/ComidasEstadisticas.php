74108473e7bce5ebdd4db512a5be497cc7fdc34b
DESKTOP-GIO8CC4\DESARROLLO INBIONOVA <bdagrosol@gmail.com>
2021-08-06 15:33:41 -0500

    IN-396 Controlador livewire

---
+++ app/Http/Livewire/Admin/UsuarioInformacion/ComidasEstadisticas.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ComidasEstadisticas extends Component
+{
+    public function render()
+    {
+        return view('livewire.comidas-estadisticas');
+    }
+}

e9c0a724dfe171525a1400f223be6650abb226a4
DESKTOP-GIO8CC4\DESARROLLO INBIONOVA <bdagrosol@gmail.com>
2021-08-09 16:25:36 -0500

    IN-396 Controlador livewire

---
+++ app/Http/Livewire/Admin/UsuarioInformacion/DeposicionesEstadisticas.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class DeposicionesEstadisticas extends Component
+{
+    public function render()
+    {
+        return view('livewire.deposiciones-estadisticas');
+    }
+}

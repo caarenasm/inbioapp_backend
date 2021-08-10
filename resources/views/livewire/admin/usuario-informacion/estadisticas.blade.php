8b2240a35c0541791179ed5e9cd54a0d4630d3af
DESKTOP-GIJ6OOM\LIZ <bdagrosol@gmail.com>
2021-08-09 21:00:55 -0500

    IN-396 Cambios en la interfaz

--- resources/views/livewire/admin/usuario-informacion/estadisticas.blade.php
+++ resources/views/livewire/admin/usuario-informacion/estadisticas.blade.php
@@ -2,8 +2,9 @@
 
     @section('title', 'informacion')
 
-        <div class="p-2 bg-white">
-            <h2 class="text-2xl py-2 text-fondo-verde font-extrabold">Informacion de los usuarios</h2>
+    <div class="p-2 bg-white">
+        <h2 class="text-2xl py-2 text-fondo-verde font-extrabold">Informacion de los usuarios</h2>
+        <div class="flex flex-grow">
             <div class="p-2 bg-white">
                 <div class="flex flex-col mb-6">
                     <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
@@ -11,5 +12,15 @@
                     </div>
                 </div>
             </div>
+            <div class="items-center">
+                <div>
+                    <a href="{{ url()->previous() }}"
+                        class="w-50 justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-color-primario hover:bg-color-primario-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-color-primario-700"
+                        type="submit">
+                        Enfermedades y señales subj.
+                    </a>
+                </div>
+            </div>
         </div>
-    </x-app-layout>
+    </div>
+</x-app-layout>

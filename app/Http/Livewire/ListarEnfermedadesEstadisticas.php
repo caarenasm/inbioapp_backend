5af4955ee6ea78ca36b9766d3ae3b96dc879bb81
DESKTOP-GIO8CC4\DESARROLLO INBIONOVA <bdagrosol@gmail.com>
2021-08-11 15:13:29 -0500

    IN-396 Controlador livewire

---
+++ app/Http/Livewire/ListarEnfermedadesEstadisticas.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ListarEnfermedadesEstadisticas extends Component
+{
+    public function render()
+    {
+        return view('livewire.listar-enfermedades-estadisticas');
+    }
+}

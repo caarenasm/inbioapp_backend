cdd89f3b72a78817c646fbeef3d8f18f1411e451
DESKTOP-GIO8CC4\DESARROLLO INBIONOVA <bdagrosol@gmail.com>
2021-08-12 11:50:16 -0500

    IN-410 Controlador livewire

---
+++ app/Http/Livewire/SeñalesEstadisticas.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class SeñalesEstadisticas extends Component
+{
+    public function render()
+    {
+        return view('livewire.señales-estadisticas');
+    }
+}

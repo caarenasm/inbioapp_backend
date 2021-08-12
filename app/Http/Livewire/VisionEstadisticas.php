b9cd7eeec89b1b1e462dac727bf7fcac5585f9ed
DESKTOP-GIO8CC4\DESARROLLO INBIONOVA <bdagrosol@gmail.com>
2021-08-12 11:50:22 -0500

    IN-410 Cambios en la interfaz de glucosaIN-410 Controlador livewire

---
+++ app/Http/Livewire/VisionEstadisticas.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class VisionEstadisticas extends Component
+{
+    public function render()
+    {
+        return view('livewire.vision-estadisticas');
+    }
+}

1fe2f79cd6bba27ce13b8bb4b03d2b670dda08ac
DESKTOP-GIO8CC4\DESARROLLO INBIONOVA <bdagrosol@gmail.com>
2021-08-20 11:56:13 -0500

    IN-419 Cambios en la ruta de la api

--- routes/api.php
+++ routes/api.php
@@ -59,6 +59,7 @@ Route::group([
             Route::post('semaforo/categoria/alimento', [EnfermedadAlimentoController::class, 'categoria_alimentos']);
             Route::post('semaforo/enfermedad/guardar', [EnfermedadAlimentoController::class, 'guardar']);
             Route::post('semaforo/alimento', [EnfermedadAlimentoController::class, 'semaforo']);
+            Route::get('semaforo/recetas', [EnfermedadAlimentoController::class, 'recetas_recomendadas']);
             Route::get('receta', [RecetaController::class, 'lista']);
             Route::get('blog', [BlogApi::class, 'lista']);
             Route::get('plan', [PlanController::class, 'lista']);

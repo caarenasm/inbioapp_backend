8c1c501a7ccdbfb9f568aa3220b43a915227c5cd
DESKTOP-GIO8CC4\DESARROLLO INBIONOVA <bdagrosol@gmail.com>
2021-08-24 09:35:21 -0500

    in-427 Se añaden los roles necesarios

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -19,9 +19,13 @@ class RoleSeeder extends Seeder
         $roleAdmin = Role::create(['name' => 'Administrador']);
         $roleEditor = Role::create(['name' => 'Editor']);
         $roleCliente = Role::create(['name' => 'Cliente']);
+        $roleNutricion = Role::create(['name' => 'Coach Nutrición']);
+        $roleHuertas = Role::create(['name' => 'Coach Huertas']);
 
         Permission::create(['name' => 'admin'])->assignRole($roleAdmin);
         Permission::create(['name' => 'editor'])->assignRole($roleEditor);
         Permission::create(['name' => 'cliente'])->assignRole($roleCliente);
+        Permission::create(['name' => 'coach_nutricion'])->assignRole($roleNutricion);
+        Permission::create(['name' => 'coach_huertas'])->assignRole($roleHuertas);
     }
 }

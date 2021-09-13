96dbae6f8b0dd4eca985a6f4217c5f72da503bd5
DESKTOP-GIO8CC4\DESARROLLO INBIONOVA <bdagrosol@gmail.com>
2021-09-13 17:09:12 -0500

    IN-435 Cambios en la interfaz de listar  recetas

--- resources/views/livewire/admin/recetas/listar-recetas.blade.php
+++ resources/views/livewire/admin/recetas/listar-recetas.blade.php
@@ -1,4 +1,4 @@
-<table id="recetas" class="display compact" style="width:100%; padding-top: 1em;  padding-bottom: 1em;">
+<table id="receta" class="display compact" style="width:100%; padding-top: 1em;  padding-bottom: 1em;">
     <thead class="bg-gray-50">
         <tr>
             <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
@@ -14,95 +14,100 @@
                 Preparación
             </th>
             <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Valores 
+                Valores
             </th>
             <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                 Acciones
         </tr>
     </thead>
     <tbody class="bg-white divide-y divide-gray-200">
-    @foreach($recetas as $receta)
-        <tr>
-            <td class="px-6 py-4 whitespace-nowrap">
-                <div class="flex items-center">
-                    <div class="ml-1">
-                        <div class="text-sm font-medium text-gray-900">
-                            <img id="picture" src="{{ asset('./imagenes/recetas/' . old('imagen_url', $receta->imagen_url)) }}" alt="receta" style="width: 200px; height: 100;">
+        @foreach ($recetas as $receta)
+            <tr>
+                <td class="px-6 py-4 whitespace-nowrap">
+                    <div class="flex items-center">
+                        <div class="ml-1">
+                            <div class="text-sm font-medium text-gray-900">
+                                <img id="picture"
+                                    src="{{ asset('./imagenes/recetas/' . old('imagen_url', $receta->imagen_url)) }}"
+                                    alt="receta" style="width: 200px; height: 100;">
+                            </div>
                         </div>
                     </div>
-                </div>
-            </td>
-            <td class="px-6 py-4 whitespace-nowrap">
-                <div class="flex items-center">
-                    <div class="ml-1">
-                        <div class="text-sm font-medium text-gray-900">
-                            {{$receta->titulo}}
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap">
+                    <div class="flex items-center">
+                        <div class="ml-1">
+                            <div class="text-sm font-medium text-gray-900">
+                                {{ $receta->titulo }}
+                            </div>
                         </div>
                     </div>
-                </div>
-            </td>
-            <td class="px-6 py-4 whitespace-nowrap">
-                <div class="flex items-center">
-                    <div class="ml-1">
-                        <div class="text-sm font-medium text-gray-900">
-                                {{strip_tags($receta->descripcion)}}
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap">
+                    <div class="flex items-center">
+                        <div class="ml-1">
+                            <div class="text-sm font-medium text-gray-900">
+                                {{ strip_tags($receta->descripcion) }}
+                            </div>
                         </div>
                     </div>
-                </div>
-            </td>
-            <td class="px-6 py-4 whitespace-nowrap">
-                <div class="flex items-center">
-                    <div class="ml-1">
-                        <div class="text-sm font-medium text-gray-900">
-                            <label for="">Caloria:</label> {{$receta->caloria}}
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap">
+                    <div class="flex items-center">
+                        <div class="ml-1">
+                            <div class="text-sm font-medium text-gray-900">
+                                <label for="">Caloria:</label> {{ $receta->caloria }}
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="flex items-center">
-                    <div class="ml-1">
-                        <div class="text-sm font-medium text-gray-900">
-                           <label for="">Grasa:</label> {{$receta->grasa}}
+                    <div class="flex items-center">
+                        <div class="ml-1">
+                            <div class="text-sm font-medium text-gray-900">
+                                <label for="">Grasa:</label> {{ $receta->grasa }}
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="flex items-center">
-                    <div class="ml-1">
-                        <div class="text-sm font-medium text-gray-900">
-                            <label for="">Proteina:</label> {{$receta->proteina}}
+                    <div class="flex items-center">
+                        <div class="ml-1">
+                            <div class="text-sm font-medium text-gray-900">
+                                <label for="">Proteina:</label> {{ $receta->proteina }}
+                            </div>
                         </div>
                     </div>
-                </div>
                 <td class="px-6 py-4 whitespace-nowrap">
                     @if ($receta->publicacion)
                         <span
                             class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                        Publicada
-                    </span>
+                            Publicada
+                        </span>
                     @else
                         <span
                             class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">
-                        No publicada
-                    </span>
+                            No publicada
+                        </span>
                     @endif
 
                 </td>
-            </td>
-            <td class="px-6 py-4 whitespace-nowrap">
-                <div class="flex items-center">
-                    <h2 class="text-2xl text-fondo-verde font-extrabold">
-                        <x-html.link href="{{route('recetas.edit',$receta)}}" text="Editar" isButton="true" class="inline-block mb-2 ml-1" />
-                    </h2>
-                    <h2 class="text-2xl text-fondo-verde font-extrabold">
-                        <x-html.link href="{{route('ingredientes.index',$receta->id)}}" text="Agregar alimentos" isButton="true" class="inline-block mb-2 ml-1" />
-                    </h2>
-                    <td class="px-2 py-4 whitespace-nowrap text-right text-sm font-medium">
-                        <a href="{{route('recetas.delete', $receta->id)}}"
-                           x-on:click="confirmDialog = confirmDialog !== true"
-                           class="justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-color-peligro hover:bg-color-peligro-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-color-peligro-300 eliminar">Eliminar</a>
-                    </td>
-                </div>
-            </td>
-        </tr>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap">
+                    <div class="flex items-center">
+                        <h2 class="text-2xl text-fondo-verde font-extrabold">
+                            <x-html.link href="{{ route('recetas.edit', $receta) }}" text="Editar" isButton="true"
+                                class="inline-block mb-2 ml-1" />
+                        </h2>
+                        <h2 class="text-2xl text-fondo-verde font-extrabold" style="margin-right: 10px">
+                            <x-html.link href="{{ route('ingredientes.index', $receta->id) }}"
+                                text="Agregar alimentos" isButton="true" class="inline-block mb-2 ml-1" />
+                        </h2>
+                        <h2 class="mb-2">
+                            <a href="{{ route('recetas.delete', $receta->id) }}"
+                                x-on:click="confirmDialog = confirmDialog !== true"
+                                class="py-2.5 px-2.5 text-sm rounded-md text-white bg-color-peligro eliminar">Eliminar</a>
+
+                        </h2>
+                    </div>
+                </td>
+            </tr>
         @endforeach
     </tbody>
 </table>
@@ -117,7 +122,7 @@
     <script>
         $(document).ready(function() {
 
-            var table = $('#recetas').DataTable({
+            var table = $('#receta').DataTable({
                     "paging": true,
                     "lengthChange": true,
                     "searching": true,

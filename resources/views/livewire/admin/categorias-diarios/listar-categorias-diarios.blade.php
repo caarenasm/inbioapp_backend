69ad1270ab4cd76316e04d980d3363a166aa10ea
INBIONOVA <bdagrosol@gmail.com>
2021-09-14 01:38:14 -0500

    IN-435 Se crean interfaz grafica para listar categoria de diario

---
+++ resources/views/livewire/admin/categorias-diarios/listar-categorias-diarios.blade.php
@@ -0,0 +1,73 @@
+<table id="categoria_diarios" class="display compact" style="width:100%; padding-top: 1em;  padding-bottom: 1em;">
+    <thead class="bg-gray-50">
+        <tr>
+            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Categorias
+            </th>
+            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Acciones
+            </th>
+        </tr>
+    </thead>
+    <tbody class="bg-white divide-y divide-gray-200">
+        @foreach ($categorias_diarios as $categoria_diario)
+            <tr>
+                <td class="px-6 py-4 whitespace-nowrap">
+                    <div class="flex items-center">
+                        <div class="ml-1">
+                            <div class="text-sm font-medium text-gray-900">
+                                {{ $categoria_diario->nombre }}
+                            </div>
+                        </div>
+                    </div>
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap">
+                    <div class="flex items-center">
+                        <h2 class="text-2xl text-fondo-verde font-extrabold" style="margin-right: 10px">
+                            <x-html.link href="{{ route('categorias-diarios.edit', $categoria_diario) }}" text="Editar"
+                                isButton="true" class="inline-block" />
+                        </h2>
+                        <h2>
+                            <a href="{{ route('categorias-diarios.delete', $categoria_diario->id) }}"
+                                x-on:click="confirmDialog = confirmDialog !== true"
+                                class=" py-2.5 px-2.5 text-sm rounded-md text-white bg-color-peligro eliminar">Eliminar</a>
+                        </h2>
+                    </div>
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
+
+</table>
+
+@push('scripts')
+    <!-- jQuery -->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+
+    <!--Datatables -->
+    <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
+    <script>
+        $(document).ready(function() {
+
+            var table = $('#categoria_diarios').DataTable({
+                    "paging": true,
+                    "lengthChange": true,
+                    "searching": true,
+                    "ordering": true,
+                    "info": true,
+                    "autoWidth": true,
+                    "lengthMenu": [
+                        [5, 10, 25, 50, -1],
+                        [5, 10, 25, 50, "Mostrar Todo"]
+                    ],
+                    responsive: true,
+                    "language": {
+                        "url": "//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json"
+                    }
+                })
+                .columns.adjust()
+                .responsive.recalc();
+        });
+    </script>
+@endpush

112dcbbf1f00ff204182924a6a800d0c22ab3e04
DESKTOP-GIO8CC4\DESARROLLO INBIONOVA <bdagrosol@gmail.com>
2021-09-14 09:36:07 -0500

    IN-435 Creacion de controlador livewire

---
+++ app/Http/Livewire/Admin/SubtipoLecturas/SubtiposLecturas.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class SubtiposLecturas extends Component
+{
+    public function render()
+    {
+        return view('livewire.subtipos-lecturas');
+    }
+}

a80e4e8e99fcfccaadde13ec95a7a54d928778d3
INBIONOVA <bdagrosol@gmail.com>
2021-09-14 01:32:11 -0500

    IN-435 Cambios en la interfaz

--- resources/views/livewire/admin/product/form-categoria.blade.php
+++ resources/views/livewire/admin/product/form-categoria.blade.php
@@ -1,18 +1,16 @@
 <div class="mb-3">
     <label for="name" class="block font-bold text-gray-700">Nombre</label>
-    <input type="text" name="name" id="name" class="w-full rounded-xl text-gray-500 border-gray-300"
-        value="{{ old('name', $category->name) }}">
+    <input type="text" name="name" id="name" class="w-full rounded-xl text-gray-500 border-gray-300" value="{{ old('name', $category->name) }}">
     @error('title')
-        <small class="text-red-500">* {{ $message }}</small>
+    <small class="text-red-500">* {{ $message }}</small>
     @enderror
 </div>
 
 <div class="mb-3">
     <label for="slug" class="block font-bold text-gray-700">Url de la categoría</label>
-    <input type="text" name="slug" id="slug" class="w-full rounded-xl text-gray-500 border-gray-300"
-        value="{{ old('slug', $category->slug) }}" readonly>
+    <input type="text" name="slug" id="slug" class="w-full rounded-xl text-gray-500 border-gray-300" value="{{ old('slug', $category->slug) }}" readonly>
     @error('slug')
-        <small class="text-red-500">* {{ $message }}</small>
+    <small class="text-red-500">* {{ $message }}</small>
     @enderror
 </div>
 
@@ -25,22 +23,22 @@
         <div>
             @if ($category->id)
                 <img src="/storage/{{$category->url}}" id="miniatura">
-            @else
-                <img src="/imagenes/blog/placeholder.png" id="miniatura">
-            @endif
-        </div>
-    </div>
+@else
+<img src="/imagenes/blog/placeholder.png" id="miniatura">
+@endif
+</div>
+</div>
 
-    @error('imagen')
-    <small class="text-red-500">* {{$message}}</small>
-    @enderror
+@error('imagen')
+<small class="text-red-500">* {{$message}}</small>
+@enderror
 </div> --}}
 <div class="w-1/2 mb-3">
     <p class="block font-bold text-gray-700">Elijé la resolución para la imagen</p>
-    <select wire:model="selectedState" class="form-control" id="dimension" name="dimension" required>
+    <select wire:model="selectedState" class="form-control rounded-xl border-gray-300" id="resolucion" name="resolucion" required>
         <option value="" selected>Escoge la resolución</option>
-        @foreach ($resoluciones as $dimension)
-            <option value="{{$dimension->id}}" @if(old('dimension') == $dimension->id || $dimension->id == $category->dimension) selected @endif> {{ $dimension->resolucion }}</option>
+        @foreach ($resoluciones as $resolucion)
+        <option value="{{$resolucion->id}}" @if(old('resolucion')==$resolucion->id || $resolucion->id == $category->resolucion) selected @endif> {{ $resolucion->resolucion }}</option>
         @endforeach
     </select>
     <small class="text-red-500"></small>
@@ -51,54 +49,55 @@
         <div class="image-wrapper">
 
             @if ($category->id)
-                <img id="picture"
-                    src="{{ asset('./imagenes/categorias_productos/' . old('imagen', $category->imagen)) }}"
-                    alt="{{ old('imagen', $category->imagen) }}">
+            <img id="picture" src="{{ asset('./imagenes/categorias_productos/' . old('imagen', $category->imagen)) }}" alt="{{ old('imagen', $category->imagen) }}">
 
             @else
-                <img id="picture" src="{{ asset('./imagenes/categorias_productos/placeholder.png') }}" alt="">
+            <img id="picture" src="{{ asset('./imagenes/categorias_productos/placeholder.png') }}" alt="">
             @endif
         </div>
     </div>
 
-    <div class="col">
-        <div class="form-group">
-            <div class="label" name="imagen">Selecciona la imagen</div>
-            <input type="file" name="imagen" id="imagen" class="form-control-file" accept="image/png, .jpeg, .jpg, image/gif"
-                value="{{asset('./imagenes/categorias_productos/' . old('imagen', $category->imagen))}}">
-        </div>
+    <div>
+        <p class="font-bold text-sgray-700">Imagen principal de la categoría</p>
+        <label for="imagen" class="block text-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-color-primario hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">Elegir
+            imagen</label>
+        <input type="file" name="imagen" id="imagen" class="hidden">
+        <p>Explicación de tamaño de imagen</p>
+
+        @error('imagen')
+        <small class="text-red-500">* {{$message}}</small>
+        @enderror
     </div>
 </div>
 
 @section('css')
-    <style>
-        .image-wrapper {
-            position: relative;
-            padding-bottom: 56.25%
-        }
+<style>
+    .image-wrapper {
+        position: relative;
+        padding-bottom: 56.25%
+    }
 
-        .image-wrapper img {
-            position: absolute;
-            object-fit: cover;
-            width: 100%;
-            height: 100%;
-        }
-
-    </style>
+    .image-wrapper img {
+        position: absolute;
+        object-fit: cover;
+        width: 100%;
+        height: 100%;
+    }
+</style>
 @endsection
 @push('scripts')
-    <script>
-        document.getElementById("imagen").addEventListener('change', cambiarImagen);
+<script>
+    document.getElementById("imagen").addEventListener('change', cambiarImagen);
 
-        function cambiarImagen(event) {
-            var file = event.target.files[0];
+    function cambiarImagen(event) {
+        var file = event.target.files[0];
 
-            var reader = new FileReader();
-            reader.onload = (event) => {
-                document.getElementById("picture").setAttribute('src', event.target.result);
-            };
+        var reader = new FileReader();
+        reader.onload = (event) => {
+            document.getElementById("picture").setAttribute('src', event.target.result);
+        };
 
-            reader.readAsDataURL(file);
-        }
-    </script>
+        reader.readAsDataURL(file);
+    }
+</script>
 @endpush

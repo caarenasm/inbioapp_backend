42b4b6cbc82b3e4427d35a251f486c665cce04a5
Yoser Perez <yoserp1@gmail.com>
2021-09-19 11:36:48 -0500

    Arreglo en consulta blog

--- app/Http/Controllers/Api/BlogApi.php
+++ app/Http/Controllers/Api/BlogApi.php
@@ -11,29 +11,27 @@ class BlogApi extends Controller
 {
     public function lista(){
 
-        $response = [];
-
-        $blogs = Blog::select('id', 'title', 'slug', 'seo_title', 'seo_description', 'image_url', 'content', 'start_date', 'end_date', 'published', 'user_id')
+        $blogs = Blog::select('id', 'title', 'slug', 'image_url', 'content', 'start_date', 'end_date')
         ->orderBy('id','asc')->get()->toArray();
 
+        $response = [];
 
         foreach ($blogs as $key => $value) {
-
-            $categorias_blogs = CategoriasBlog::leftjoin('blog_categorias_blog', 'categorias_blog_id', '=', 'categorias_blogs.id')
+            $categoria = CategoriasBlog::leftjoin('blog_categorias_blog', 'categorias_blog_id', '=', 'categorias_blogs.id')
             ->select('categorias_blogs.id', 'name', 'slug')->where('blog_id','=',$value['id'])->orderBy('id','asc')
             ->get()->toArray();
 
-            $blogs[$key]['categorias_blogs'] = $categorias_blogs;
-            $response = $blogs;
+            $response[$key]['id'] = $value['id'];
+            $response[$key]['title'] = $value['title'];
+            $response[$key]['content'] = $value['content'];
+            $response[$key]['start_date'] = $value['start_date'];
+            $response[$key]['image_url'] = asset('imagenes/blog/' . $value['image_url']);
+            $response[$key]['categoria'] = $categoria;
 
-            $response = [];
+        }
 
-            }
-
-            return response([
-                'data' => $response
-            ]);
-            
+        return response()->json($response);
+        
     }
 
     public function lista_categorias(){

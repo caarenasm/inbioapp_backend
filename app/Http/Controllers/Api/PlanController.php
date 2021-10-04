e20619b4aa7d32c6b2399d2400df6e387fc6c5a8
Yoser Perez <yoserp1@gmail.com>
2021-10-04 13:42:36 -0500

    vista planes

--- app/Http/Controllers/Api/PlanController.php
+++ app/Http/Controllers/Api/PlanController.php
@@ -16,10 +16,10 @@ use App\Models\PlanUser;
 class PlanController extends Controller
 {
 
-    public function user_plan(Request $request){
+    public function guardar(Request $request){
 
         $validator = Validator::make($request->all(), [
-            'user_id' => 'required',
+            /*'user_id' => 'required',*/
             'plan_id' => 'required',
         ]);
 
@@ -36,28 +36,26 @@ class PlanController extends Controller
         try {
 
             $user_plan = New PlanUser;
-
-            $user_plan->user_id = $request->user_id;
-            // $user_plan->user_id = Auth::$id;
+            /*$user_objetivo->user_id = $request->user_id;*/
+            $user_plan->user_id = $request->user()->id;
             $user_plan->plan_id = $request->plan_id;
             $user_plan->save();
 
             DB::commit();
 
             return response()->json([
-                'message' => 'Usuario creado con exito!',
-                'data' => $request->all()
+                'message' => 'Datos guardados con exito!',
+                'status' => true
             ], 200);
 
         }catch (\Illuminate\Database\QueryException $e){
 
             DB::rollback();
 
-            /*$response['errors']  = array('ERROR ('.$e->getCode().'):'=> $e->getMessage());
-
-            return response()->json([
-                $response
-            ], 400);*/
+            // $response['errors']  = array('ERROR ('.$e->getCode().'):'=> $e->getMessage());
+            // return response()->json([
+            //     $response
+            // ], 400);
 
             return response()->json([
                 'message' => 'Error en operacion!'
